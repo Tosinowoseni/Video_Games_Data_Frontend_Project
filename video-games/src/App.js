73468368904cdaf51131b414fbae9a24e6591322
tosinowoseni <tosinowosen1@icloud.com>
2022-04-27 14:21:25 -0400

    Created Axios Request.js and two New Files in The Components Folder

--- video-games/src/App.js
+++ video-games/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import SuccessfulConsolesChart from "./Components/SuccesfulConsolesChart";
+import { getById, makeGetRequest } from "./AxiosRequests";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [requestReload, setRequestReload] = useState(true);
+    const [barChartData, setBarChartData] = useState([]);
+    const [videoGames, setVideoGames] = useState([]);
+
+    useEffect(() => {
+        if (requestReload) {
+            //makeGetRequest();
+            getGameSalesByConsole();
+            setRequestReload(false);
+        }
+    }, [requestReload]);
+
+    //https://localhost:7260/api/games/gamesByConsole
+    async function getGameSalesByConsole() {
+        try {
+            let response = await axios.get(
+                `https://localhost:7260/api/games/gamesByConsole`
+            );
+            setBarChartData(response.data);
+        } catch (ex) {
+            console.log("Oh no something didn't work right :(");
+        }
+    }
+    //returns one game from pk
+    async function getById(id) {
+        try {
+            let response = await axios.get(`https://localhost:7260/api/games/${id}`);
+            console.log(response.data);
+            setVideoGames(response.data);
+        } catch (ex) {
+            console.log("Oh no something didn't work right :(");
+        }
+    }
+
+    //returns all games
+    async function makeGetRequest() {
+        try {
+            let response = await axios.get(`https://localhost:7260/api/games/`);
+            console.log(response.data);
+            setVideoGames(response.data);
+        } catch (ex) {
+            console.log("Oh no something didn't work right :(");
+        }
+    }
+
+    return (
+        <div>
+            <h2>Video Games</h2>
+            <SuccessfulConsolesChart
+                chartData={barChartData}
+                getRequest={getGameSalesByConsole}
+            />
+        </div>
+    );
 }
 
 export default App;
