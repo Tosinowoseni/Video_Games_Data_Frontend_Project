1d2a964d7609905142fe027b4de66ab1e5d2f892
tosinowoseni <tosinowosen1@icloud.com>
2022-04-27 17:04:46 -0400

    Created Table Components + SearchBar

--- video-games/src/App.js
+++ video-games/src/App.js
@@ -1,36 +1,45 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+//Components VVV
+import Header from "./Components/header";
 import SuccessfulConsolesChart from "./Components/SuccesfulConsolesChart";
-import { getById, makeGetRequest } from "./AxiosRequests";
+import Table from "./Components/Table/Table";
+//Useless import VVV
+import { getById, makeGetRequest } from "./services/AxiosRequests";
 
 function App() {
     const [requestReload, setRequestReload] = useState(true);
-    const [barChartData, setBarChartData] = useState([]);
     const [videoGames, setVideoGames] = useState([]);
+    const [barChartData, setBarChartData] = useState([]);
 
-    useEffect(() => {
-        if (requestReload) {
-            //makeGetRequest();
-            getGameSalesByConsole();
-            setRequestReload(false);
-        }
-    }, [requestReload]);
-
-    //https://localhost:7260/api/games/gamesByConsole
     async function getGameSalesByConsole() {
         try {
             let response = await axios.get(
-                `https://localhost:7260/api/games/gamesByConsole`
+                "https://localhost:7260/api/games/gamesByConsole"
             );
+            console.log("Get Games Sales response: ", response.data);
             setBarChartData(response.data);
         } catch (ex) {
-            console.log("Oh no something didn't work right :(");
+            console.log("Oh no something didn't work right :(", ex);
         }
     }
-    //returns one game from pk
-    async function getById(id) {
+
+    //returns one game from pk !!!!!!!!!!!UNUSED!!!!!!!!!!!
+    // async function getById(id) {
+    //   try {
+    //     let response = await axios.get(`https://localhost:7260/api/games/${id}`);
+    //     console.log(response.data);
+    //     setVideoGames(response.data);
+    //   } catch (ex) {
+    //     console.log("Oh no something didn't work right :(");
+    //   }
+    // }
+
+    async function getByName(name) {
         try {
-            let response = await axios.get(`https://localhost:7260/api/games/${id}`);
+            let response = await axios.get(
+                `https://localhost:7260/api/games/gamebyname/${name}`
+            );
             console.log(response.data);
             setVideoGames(response.data);
         } catch (ex) {
@@ -38,11 +47,23 @@ function App() {
         }
     }
 
-    //returns all games
-    async function makeGetRequest() {
+    //returns all games !!!!!!!!!!!UNUSED!!!!!!!!!!!
+    // async function makeGetRequest() {
+    //   try {
+    //     let response = await axios.get(`https://localhost:7260/api/games/`);
+    //     console.log(response.data);
+    //     setVideoGames(response.data);
+    //   } catch (ex) {
+    //     console.log("Oh no something didn't work right :(");
+    //   }
+    // }
+
+    async function makeGetRequestBySearch(searchTerm) {
         try {
-            let response = await axios.get(`https://localhost:7260/api/games/`);
-            console.log(response.data);
+            let response = await axios.get(
+                `https://localhost:7260/api/games/search/${searchTerm}`
+            );
+            console.log("This came from search bar!", response.data);
             setVideoGames(response.data);
         } catch (ex) {
             console.log("Oh no something didn't work right :(");
@@ -51,11 +72,13 @@ function App() {
 
     return (
         <div>
+            <Header getSearch={makeGetRequestBySearch} />
             <h2>Video Games</h2>
             <SuccessfulConsolesChart
-                chartData={barChartData}
-                getRequest={getGameSalesByConsole}
+                getData={getGameSalesByConsole}
+                data={barChartData}
             />
+            <Table videoGames={videoGames} getByName={getByName} />
         </div>
     );
 }
